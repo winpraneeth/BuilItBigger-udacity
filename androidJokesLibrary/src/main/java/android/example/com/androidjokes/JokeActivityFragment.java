76ac260b9c7bfca1e5c8fc353879bdb78d4d9bc3
Praneeth Vinjamuri <praneethvin.09@gmail.com>
2016-05-02 10:19:56 -0500

    Removed debug statements

--- androidJokesLibrary/src/main/java/android/example/com/androidjokes/JokeActivityFragment.java
+++ androidJokesLibrary/src/main/java/android/example/com/androidjokes/JokeActivityFragment.java
@@ -1,13 +1,12 @@
 package android.example.com.androidjokes;
 
         import android.content.Intent;
-        import android.support.v4.app.Fragment;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,7 +23,6 @@ public class JokeActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_joke, container, false);
         Intent intent = getActivity().getIntent();
-        Log.d(LOG_TAG, "you are here");
         String joke = intent.getStringExtra(JokeActivity.TAG_JOKE);
         TextView jokeTextView = (TextView) rootView.findViewById(R.id.joke_textView);
         if (joke != null && joke.length() != 0) {
