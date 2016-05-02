c77b7b2f4f1a5599a08cc667b869959aa7d6aa18
Praneeth Vinjamuri <praneethvin.09@gmail.com>
2016-05-02 10:08:58 -0500

    Initial Commit and Java Jokes Library
    Step 1

---
+++ javajokeslibrary/src/main/java/com/example/JavaJokes.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class JavaJokes {
+    public String getJoke() {
+        return "Ordinarily, staring is creepy. But if you spread your attention across many individuals, then it's just people watching.";
+    }
+}
